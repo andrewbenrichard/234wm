b34780fdf6f723db9fccd80253755a9a13c85b1a
Andrew Ben Richard <andrewbenrichard@gmail.com>
2020-08-25 13:30:21 +0100

    fixes

--- app/Http/Controllers/API/ApiController.php
+++ app/Http/Controllers/API/ApiController.php
@@ -126,10 +126,10 @@ class ApiController extends Controller
         $plan = Plan::get();
 
        
-        $response[]=$plan
-     );
+        $response[]=$plan;
+     
      $set['234WM_API_V1']  = $response;
-       
+        
       
      header( 'Content-Type: application/json; charset=utf-8' );
      echo $val= str_replace('\\/', '/', json_encode($set,JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
